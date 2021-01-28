99fc13343232cc4469dfde27f9e01881ccee7d26
jeffreytse <jeffreytse@users.noreply.github.com>
2021-01-28 10:28:44 +0000

    jekyll build from Action e7c50042253d9b581efa1ff945cd4533961848c4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Test post",
+        "excerpt":"Just a test  ","categories": [],
+        "tags": [],
+        "url": "https://jeffreytse.github.io/ivansiu.com/2021/01/27/test-post",
+        "teaser": "https://jeffreytse.github.io/ivansiu.com/images/teaser.jpg"
+      },{
+        "title": "Test post 2",
+        "excerpt":"Test post 2  ","categories": [],
+        "tags": [],
+        "url": "https://jeffreytse.github.io/ivansiu.com/2021/01/27/test-post-2",
+        "teaser": "https://jeffreytse.github.io/ivansiu.com/images/teaser.jpg"
+      },{
+        "title": "Test post 3",
+        "excerpt":"Test post 3  ","categories": [],
+        "tags": [],
+        "url": "https://jeffreytse.github.io/ivansiu.com/2021/01/27/test-post-3",
+        "teaser": "https://jeffreytse.github.io/ivansiu.com/images/teaser.jpg"
+      }]
